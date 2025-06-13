c63c5a59e3a6b34cec5c2c058ac9ff622868db20
valeriy <komp.val2404@gmail.com>
2025-06-13 23:54:32 +0300

    add files

---
+++ proxy_server/include/logger.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+
+class Logger {
+public:
+    enum class LogLevel {
+        DEBUG,
+        INFO,
+        WARNING,
+        ERROR
+    };
+
+    static Logger& get_instance();
+
+    void debug(const std::string& message);
+    void info(const std::string& message);
+    void warning(const std::string& message);
+    void error(const std::string& message);
+
+private:
+    Logger();
+    ~Logger();
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+
+    void log(LogLevel level, const std::string& message);
+    std::string get_timestamp();
+
+    std::ofstream log_file_;
+}; 
---
+++ proxy_server/src/logger.cpp
@@ -0,0 +1,74 @@
+#include "logger.hpp"
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+
+Logger& Logger::get_instance() {
+    static Logger instance;
+    return instance;
+}
+
+Logger::Logger() {
+    log_file_.open("proxy.log", std::ios::app);
+}
+
+Logger::~Logger() {
+    if (log_file_.is_open()) {
+        log_file_.close();
+    }
+}
+
+std::string Logger::get_timestamp() {
+    auto now = std::chrono::system_clock::now();
+    auto time = std::chrono::system_clock::to_time_t(now);
+    std::stringstream ss;
+    ss << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S");
+    return ss.str();
+}
+
+void Logger::log(LogLevel level, const std::string& message) {
+    std::string level_str;
+    switch (level) {
+        case LogLevel::DEBUG:
+            level_str = "DEBUG";
+            break;
+        case LogLevel::INFO:
+            level_str = "INFO";
+            break;
+        case LogLevel::WARNING:
+            level_str = "WARNING";
+            break;
+        case LogLevel::ERROR:
+            level_str = "ERROR";
+            break;
+    }
+
+    std::string log_message = get_timestamp() + " [" + level_str + "] " + message;
+
+    // Console output
+    std::cout << log_message << std::endl;
+
+    // File output
+    if (log_file_.is_open()) {
+        log_file_ << log_message << std::endl;
+        log_file_.flush();
+    }
+}
+
+void Logger::debug(const std::string& message) {
+    log(LogLevel::DEBUG, message);
+}
+
+void Logger::info(const std::string& message) {
+    log(LogLevel::INFO, message);
+}
+
+void Logger::warning(const std::string& message) {
+    log(LogLevel::WARNING, message);
+}
+
+void Logger::error(const std::string& message) {
+    log(LogLevel::ERROR, message);
+} 
